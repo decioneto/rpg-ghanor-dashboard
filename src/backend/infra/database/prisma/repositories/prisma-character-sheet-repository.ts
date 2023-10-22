e08dfed3367d69537f80d3bc1a6e9ac060682284
Décio Neto <decio.neeto@gmail.com>
2023-10-22 11:39:42 -0300

    create ch sheet prisma repositoty

--- src/backend/infra/database/prisma/repositories/prisma-character-sheet-repository.ts
+++ src/backend/infra/database/prisma/repositories/prisma-character-sheet-repository.ts
@@ -1,4 +1,5 @@
 import { AttributeName } from '@/backend/core/entities/attr-name';
+import { CharacterSheet } from '@/backend/core/entities/ch-sheet';
 import { AttributeValueModel } from '@/backend/data/models/attr-value';
 import { CharacterSheetRepository } from '@/backend/data/repositories/character-sheet-repository';
 import { PrismaService } from '../prisma-service';
@@ -7,6 +8,22 @@ export class PrismaCharacterSheetRepository
     implements CharacterSheetRepository
 {
     constructor(private prismaService: PrismaService) {}
+
+    async createChSheet(chSheet: CharacterSheet): Promise<boolean> {
+        const result = await this.prismaService.characterSheet.create({
+            data: {
+                chName: chSheet.chName,
+                chLevel: chSheet.chLevel,
+                user: {
+                    connect: {
+                        id: chSheet.userId,
+                    },
+                },
+            },
+        });
+
+        return result.id !== null;
+    }
 
     async createAttrName(attrName: AttributeName): Promise<boolean> {
         const result = await this.prismaService.attributesName.create({
